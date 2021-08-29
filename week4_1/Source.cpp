2e80040eb2eebdf840a9548f1b541d17858097e0
NoNnO7 <non.praisri9029@gmail.com>
2021-08-29 19:12:28 +0700

    Add project files.

---
+++ week4_1/Source.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main() {
+	int x, y, n;
+	scanf_s("%d", &n);
+	x = 1;
+	while (x <= n)
+	{
+		printf("*");
+		x++;
+	}
+	printf("\n");
+	x = 1;
+	while (x <= n-2) {
+		y = 1;
+		printf("*");
+		while (y <= n-2) {
+			printf("*");
+			y++;
+		}
+		printf("*\n");
+		x++;
+	}
+	x = 1;
+	while (x <= n)
+	{
+		printf("*");
+		x++;
+	}
+	return 0;
+}
